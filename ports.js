3370c171d025ca3e5b7e462da30e2311eeb62008
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-03-17 16:38:57 +0100

    Updates

---
+++ ports.js
@@ -0,0 +1,47 @@
+const KINTO_SERVER = "https://kinto.dev.mozaws.net/v1";  // No trailing
+
+// Handle redirection with the bearer token in the hash
+if (window.location.hash.indexOf("#auth=") === 0) {
+  const token = window.location.hash.split('#auth=')[1];
+  window.location.hash = "";
+  localStorage.setItem("bearer", token);
+}
+
+// Retrieve saved state.
+const bearer = localStorage.getItem("bearer");
+const email = localStorage.getItem("email");
+
+app = Elm.Main.fullscreen({email: email, bearer: bearer});
+
+// Handle calling to Kinto-Portier login resource.
+app.ports.authenticate.subscribe(function(emailValue) {
+  const {origin, pathname} = document.location;
+  const form = document.createElement("form");
+  const email = document.createElement("input");
+  const redirect = document.createElement("input");
+
+  form.method = "POST";
+  form.action = `${KINTO_SERVER}/portier/login`;
+  form.style.display = "none";
+
+  email.name = "email";
+  email.value = emailValue;
+  form.appendChild(email);
+
+  redirect.value = `${origin}${pathname}#auth=`;
+  redirect.name = "redirect";
+  form.appendChild(redirect);
+
+  document.body.appendChild(form);
+  form.submit();
+});
+
+
+// Handle storing some state information
+app.ports.saveData.subscribe(function(data) {
+  if (data.value === null) {
+    localStorage.removeItem(data.key);
+  } else {
+    localStorage.setItem(data.key, data.value);
+  }
+});
