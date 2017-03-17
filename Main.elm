acd1ac204fb9e0ee7aa1c4d529986757a8c793b3
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-03-17 15:33:17 +0100

    Setup Elm app basics.

--- Main.elm
+++ Main.elm
@@ -3,5 +3,67 @@ module Main exposing (..)
 import Html
 
 
+-- Model
+
+
+type alias Model =
+    { email : String }
+
+
+
+-- Msg
+
+
+type Msg
+    = NewEmail String
+
+
+
+-- Init
+
+
+init : ( Model, Cmd Msg )
+init =
+    { email = "" } ! []
+
+
+
+-- Subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- Updates
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        NewEmail email ->
+            { model | email = email } ! []
+
+
+
+-- View
+
+
+view : Model -> Html.Html Msg
+view model =
+    Html.div [] [ Html.text "Manual StepFunction Dashboard" ]
+
+
+
+-- Main
+
+
 main =
-    Html.text "hello world"
+    Html.program
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        }
